22a28b76a652da59192bb4b81b5553692302b7ff
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-01-22 02:07:13 -0500

    ugh. wasted JS time...

--- js/ookb.js
+++ js/ookb.js
@@ -16,13 +16,24 @@
   "use strict"; // jshint ;_;
 
   // Some Custom JS for the site
-  // scroll to content when you click on the logo. - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
+  // Several different hide/show events...
+  
+/*
   $(document).ready(function() {
     $("section.toggle > article ").hide();
     });
-	
+*/
+	//click on title of section to hide/show
   $("section.toggle h2").click(function () {
     $(this).parent().children("article").toggle("slow");
+    });    
+
+  //expand and contract all buttons
+  $("#expansion").click(function () {
+    $("section.toggle article").show("slow");
+    });    
+  $("#contraction").click(function () {
+    $("section.toggle article").hide("slow");
     });    
 
 /*
